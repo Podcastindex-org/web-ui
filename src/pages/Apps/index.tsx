579f804003281149176d60b3d9e8088478da8a47
Comic Strip Blogger <70763580+ComicStrip@users.noreply.github.com>
2020-11-21 13:20:30 +0100

    release 001

--- src/pages/Apps/index.tsx
+++ src/pages/Apps/index.tsx
@@ -1,1 +1,72 @@
-//
+import * as React from 'react'
+import { Stats } from 'webpack'
+
+import './styles.scss'
+
+interface IProps {}
+
+export default class Card extends React.Component<IProps> {
+    static defaultProps = {}
+
+    constructor(props: IProps) {
+        super(props)
+    }
+
+    renderOld() {
+        return (
+          <pre>{`
+          old content (to be replaced with React constructs soon):
+
+          # Application Support by Element
+
+For elements that are included in the official [DTD](https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md), we will highlight applications offering production support. Links should point to a public announcement or production example.
+
+# Elements
+
+## Transcript <podcast:transcript>
+1. [Buzzsprout](https://www.buzzsprout.com/blog/transcription-tools)
+2. [Podcast Addict](https://podcastaddict.com/changelog/2020_14#section1)
+3. [Podfriend](https://twitter.com/GoPodfriend/status/1316847938828742656)
+4. [Podnews](https://podnews.net/rss)
+5. [Podcastindex](https://podcastindex.org)
+
+## Locked <podcast:locked>
+1. [Buzzsprout](https://www.buzzsprout.com/blog/podcast-locking)
+2. [Podnews](https://podnews.net/rss)
+3. [Podcastindex](https://podcastindex.org)
+
+## Funding <podcast:funding>
+1. [Buzzsprout](https://www.buzzsprout.com/)
+2. [Podfriend](https://twitter.com/GoPodfriend/status/1318663369684123648)
+3. [Podverse](https://twitter.com/Podverse/status/1325734448608899072?s=20)
+4. [Podcastindex](https://podcastindex.org)
+5. [podStation](https://chrome.google.com/webstore/detail/podstation-podcast-player/bpcagekijmfcocgjlnnhpdogbplajjfn)
+
+## Chapters <podcast:chapters>
+1. [Podcast Chapters](https://chaptersapp.com/faq/jsonExport.html)
+2. [Podfriend](https://twitter.com/adamcurry/status/1323417144378445831)
+3. [Hypercatcher](https://hypercatcher.com/2020/11/how-to-create-podcast-chapters-as-a-listener/)
+4. [PodcastAddict](https://podcastaddict.com/changelog/2020_16#section4)
+5. [Buzzsprout](https://twitter.com/buzzsprout/status/1324425781330796545)
+6. [Podverse](https://twitter.com/Podverse/status/1324729288281632771?s=20)
+7. [Podcastindex](https://podcastindex.org)
+
+## Soundbites <podcast:soundbite>
+1. [Buzzsprout](https://www.buzzsprout.com)
+2. [Podverse](https://twitter.com/Podverse/status/1326072379597053953?s=20)
+          `}</pre>
+        )
+    }
+
+    render() {
+        const {} = this.props
+        return (
+            <div className="landing-content" style={{ marginTop: 20 }}>
+
+  <div className="csb">under dev by CSB, release 001</div> 
+            {this.renderOld()}
+       
+         </div>
+        )
+    }
+}
