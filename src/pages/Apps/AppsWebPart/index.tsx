880b0c7b550ba7e6c225b88558ba4bc84fefdd5e
Comic Strip Blogger <70763580+ComicStrip@users.noreply.github.com>
2020-11-21 17:33:48 +0100

    apps release 002

--- src/pages/Apps/AppsWebPart/index.tsx
+++ src/pages/Apps/AppsWebPart/index.tsx
@@ -1,1 +1,64 @@
-//
+import * as React from 'react'
+import Podcast from '../../Podcast';
+
+
+import './styles.scss'
+
+
+interface AppsWebPartProps {
+}
+
+interface AppsWebPartState {
+    apps: Array<any>
+}
+
+
+export default class AppsWebPart extends React.Component<AppsWebPartProps,AppsWebPartState> {
+
+    constructor(props: AppsWebPartProps) {
+        super(props);
+        this.state = {
+            apps: []
+        }
+    }
+
+    _isMounted = false;
+
+    async componentDidMount() {
+        this._isMounted = true;
+        console.log("componentDidMount(): loading from JSON:")
+        const apps = await this.getApps();
+        console.log("fetched apps=", apps);
+        if (this._isMounted) {
+            this.setState({
+                apps
+            })
+        }
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false
+    }
+
+    async getApps() {
+        let response = await fetch(`/api/apps`, {
+            credentials: 'same-origin',
+            method: 'GET',
+        })
+        return await response.json()
+    }
+
+    render() {
+        return (
+            <div className="podcastIndexAppsWebPart">
+               apps loaded from JSON:
+               { this.state.apps.map((app,i) => (
+                 <div className="podcastIndexApp" key={`${i}`}>
+                     {app.appName}
+                 </div>
+                ))
+               }
+            </div>
+        )
+    }
+}
