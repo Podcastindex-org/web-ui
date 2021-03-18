2e89a250b9f3751affc1946a32a4572d20a28a6d
Dave Jones <sect62@gmail.com>
2021-03-17 21:32:37 -0500

    add totals to stats page

--- ui/src/pages/Stats/index.tsx
+++ ui/src/pages/Stats/index.tsx
@@ -2,8 +2,10 @@ import * as React from 'react'
 import { Stats } from 'webpack'
 
 import NewFeedStatsCard from './NewFeedStatsCard'
+import StatsCard from "./StatsCard";
 
 import './styles.scss'
+
 
 interface IProps {}
 
@@ -13,18 +15,25 @@ export default class Card extends React.Component<IProps> {
     static defaultProps = {}
     state = {
         loading: true,
+        overallStats: {
+            feedCountTotal: '1,318,328',
+            feedCount3days: '81,919',
+            feedCount10days: '208,264',
+            feedCount30days: '303,007',
+            feedCount60days: '376,576',
+        },
         stats: {
-            totalCount: 0,
+            totalCount: 1619,
             top1name: '',
-            top1count: 0,
+            top1count: 473,
             top2name: '',
-            top2count: 0,
+            top2count: 208,
             top3name: '',
-            top3count: 0,
+            top3count: 113,
             top4name: '',
-            top4count: 0,
+            top4count: 86,
             top5name: '',
-            top5count: 0
+            top5count: 39
         }
     }
     _isMounted = false
@@ -36,11 +45,13 @@ export default class Card extends React.Component<IProps> {
     async componentDidMount(): Promise<void> {
         this._isMounted = true
         const stats = await this.getNewFeedStats()
+        const overallStats = await this.getStats()
 
         //console.log(stats)
         if (this._isMounted) {
             this.setState({
                 loading: false,
+                overallStats,
                 stats
             })
         }
@@ -54,12 +65,27 @@ export default class Card extends React.Component<IProps> {
         return await response.json()
     }
 
+    async getStats() {
+        let response = await fetch('/api/stats', {
+            credentials: 'same-origin',
+            method: 'GET',
+        })
+        return await response.json()
+    }
+
     render() {
         const {} = this.props
-        const { loading, stats } = this.state
+        const { loading, overallStats, stats } = this.state
         return (
             <div className="landing-content" style={{ marginTop: 20 }}>
-                {/* TODO more stats soon */}
+                <StatsCard
+                    total={overallStats.feedCountTotal}
+                    threedays={overallStats.feedCount3days}
+                    tendays={overallStats.feedCount10days}
+                    lastMonth={overallStats.feedCount30days}
+                    last60={overallStats.feedCount60days}
+                />
+                <div className="info-section">&nbsp;</div>
                 <NewFeedStatsCard
                     total={stats.totalCount}
                     top1name={stats.top1name}
