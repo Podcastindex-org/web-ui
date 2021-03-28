2c7a346dd412c7d93e37719ba157b729683c46cf
Steven Crader <stevencrader@gmail.com>
2021-03-28 16:34:17 -0700

    Add link to data file and note to create PR.
    Update link color to match app links.
    
    Possibly resolves #49

--- ui/src/pages/Apps/index.tsx
+++ ui/src/pages/Apps/index.tsx
@@ -17,8 +17,23 @@ export default class Apps extends React.Component<IProps> {
         const {} = this.props
         return (
             <div className="landing-content" style={{ marginTop: 20 }}>
-            <AppsWebPart/>
-         </div>
+                <AppsWebPart/>
+                <div className="contribute-notes">
+                    <h4>Your application or website missing?</h4>
+                    <p>
+                        {'Create a '}
+                        <a href="https://github.com/Podcastindex-org/web-ui/pulls">Pull Request</a>
+                        {' to update the '}
+                        <a href="https://github.com/Podcastindex-org/web-ui/blob/master/server/data/apps.json">data file</a>.
+                    </p>
+                    <p>
+                        {'Also support namespace features? Add your app or website to the '}
+                        <a href="https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/element-support.md">application list</a>
+                        {' by creating a '}
+                        <a href="https://github.com/Podcastindex-org/podcast-namespace/pulls">Pull Request</a>.
+                    </p>
+                </div>
+            </div>
         )
     }
 }
