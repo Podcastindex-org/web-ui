21a4700012366c376aeddbce2a7437f9aaea4c66
Dave Jones <sect62@gmail.com>
2021-04-28 15:14:05 -0500

    space between html element stripping

--- ui/src/components/EpisodeItem/index.tsx
+++ ui/src/components/EpisodeItem/index.tsx
@@ -140,7 +140,7 @@ export default class EpisodeItem extends React.PureComponent<IProps> {
                         onClick={this.togglePlayPause}/>
                 </div>
                 <p className="episode-description">
-                    {truncateString(description).replace(/(<([^>]+)>)/gi, "")}
+                    {truncateString(description).replace(/(<([^>]+)>)/gi, " ")}
                 </p>
             </div>
         )
