7d50d5fda5c8ec5a39f3e1106c74b958b5617b4a
Steven Crader <stevencrader@gmail.com>
2021-05-17 20:29:51 -0700

    Add function to fix urls not starting with `http`
    If not included, adds `http://`

--- ui/src/utils.ts
+++ ui/src/utils.ts
@@ -24,16 +24,30 @@ export const truncateString = (input: string) => {
 }
 
 export const titleizeString = (input: string) => {
-  return input
-    .split(/\W+/gi)
-    .map(w => w.charAt(0).toUpperCase() + w.slice(1))
-    .join(' ')
+    return input
+        .split(/\W+/gi)
+        .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+        .join(' ')
 }
 
 export const getPrettyDate = (time: number, includeTime: boolean = true) => {
     // Javascript epoch is in milliseconds; datePublished is in seconds
-    const dateObj = new Date(time*1000)
+    const dateObj = new Date(time * 1000)
     if (includeTime)
         return dateObj.toLocaleString()
     return dateObj.toLocaleDateString()
+}
+
+export const fixURL = (url: string) => {
+    if (url === undefined || url === null) {
+        return url
+    } else {
+        url = url.trim()
+        if (url !== '') {
+            let prefix = url.substring(0, 4).toLowerCase()
+            if (prefix !== 'http')
+                url = `http://${url}`
+        }
+    }
+    return url
 }
