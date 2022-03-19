9ed0c35fa95acefd4e4338b505ad36776ad82daa
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2022-03-18 21:29:14 -0400

    More font improvement

--- webpack.config.js
+++ webpack.config.js
@@ -98,6 +98,17 @@ module.exports = {
             {
                 test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext][query]',
+                },
+                // use: {
+                //     loader: 'file-loader',
+                //     options: {
+                //         limit: 50000,
+                //         name: './fonts/[name].[ext]', // Output below ./fonts
+                //         publicPath: '../', // Take the directory into account
+                //     },
+                // },
             },
         ],
     },
