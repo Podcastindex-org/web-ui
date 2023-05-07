09396eb87e42ece3d05413e265c7af83c897e26e
Guilherme Dellagustin <dellagustin@gmail.com>
2023-05-07 10:48:58 +0200

    PR Review #233 - Comments - fix attribute errors
    References:
    -
    https://github.com/Podcastindex-org/web-ui/pull/253#discussion_r1185745312
    
    Co-authored-by: Steven Crader <stevencrader@gmail.com>

--- ui/src/components/Comments/index.tsx
+++ ui/src/components/Comments/index.tsx
@@ -69,8 +69,8 @@ class Comment extends React.PureComponent<ICommentProps, ICommentState> {
                         <time>{this.props.comment.publishedAt.toLocaleString()}</time>
                     </a>
                     <button className="context-menu" aria-label="More" onClick={() => this.onClickShowMenu()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="24" height="24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="24" height="24" strokeWidth="1.5" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                         </svg>
                         { this.state.showMenu && 
                             <CommentMenu 
